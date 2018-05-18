71dd81fc95beb2773c65bd4be52480609f940c6c
wassname <ubuntu_ip_172_31_31_38@wassname.org>
2018-05-18 11:00:54 +0000

    rnn

---
+++ world_models_sonic/models/rnn.py
@@ -0,0 +1,127 @@
+import torch
+import torch.nn as nn
+from torch.nn import functional as F
+from torch import normal, multinomial
+import torch.distributions
+from torch.autograd import Variable
+import math
+import numpy as np
+
+eps = 1e-8
+logeps = math.log(eps)
+
+class MDNRNN(nn.Module):
+    def __init__(self, z_dim, action_dim, hidden_size, n_mixture, temperature):
+        """
+            :param z_dim: the dimension of VAE latent variable
+            :param hidden_size: hidden size of RNN
+            :param n_mixture: number of Gaussian Mixture Models to be used
+            :param temperature: controls the randomness of the model
+
+            MDNRNN stands for Mixture Density Network - RNN.
+            The output of this model is [mean, sigma^2, K],
+            where mean and sigma^2 have z_dim * n_mixture elements and
+            K has n_mixture elements.
+        """
+        super(MDNRNN, self).__init__()
+        # define rnn
+        self.inpt_size = z_dim + action_dim
+        self.hidden_size = hidden_size
+        self.n_mixture = n_mixture
+        self.z_dim = z_dim
+        self.rnn = nn.LSTM(input_size=self.inpt_size, hidden_size=hidden_size, batch_first=True)
+
+        # define MDN as fully connected layer
+        self.mdn = nn.Linear(hidden_size, n_mixture * z_dim * 3)
+        self.tau = temperature
+
+    def forward(self, inpt, action, hidden_state=None):
+        """
+        :param inpt: a tensor of size (batch_size, seq_len, D)
+        :param hidden_state: two tensors of size (1, batch_size, hidden_size)
+        :param action: a tensor of (batch_size, seq_len, action_dim)
+        :return: pi, mean, sigma, hidden_state
+        """
+        batch_size, seq_len, _ = inpt.size()
+        if hidden_state is None:
+            # use new so that we do not need to know the tensor type explicitly.
+            hidden_state = (Variable(inpt.data.new(1, batch_size, self.hidden_size)),
+                            Variable(inpt.data.new(1, batch_size, self.hidden_size)))
+
+        # concatenate input and action, maybe we can use an extra fc layer to project action to a space same
+        # as inpt?
+        concat = torch.cat((inpt, action), dim=-1)
+        output, hidden_state = self.rnn(concat, hidden_state)
+        pi, mean, sigma = self.get_mixture_coef(output)
+        return pi, mean, sigma, hidden_state
+
+    def get_mixture_coef(self, output):
+        batch_size, seq_len, _ = output.size()
+        # N, seq_len, n_mixture * z_dim * 2 + n_mixture
+        output = output.contiguous()
+        output = output.view(-1, self.hidden_size)
+        
+        mixture = self.mdn(output)
+        mixture = mixture.view(batch_size, seq_len, -1)
+        
+        ## Split output into mean, logsigma, pi
+        
+        # N * seq_len
+        mu = mixture[..., :self.n_mixture * self.z_dim]
+        logsigma = mixture[..., self.n_mixture * self.z_dim: self.n_mixture * self.z_dim*2]
+        pi = mixture[..., self.n_mixture * self.z_dim*2:self.n_mixture * self.z_dim*3]
+        
+        # Reshape
+        mu = mu.view((-1, seq_len, self.n_mixture, self.z_dim))
+        logsigma = logsigma.view((-1, seq_len, self.n_mixture, self.z_dim)).clamp(np.log(eps),-np.log(eps))
+        pi = pi.view((-1, seq_len, self.n_mixture, self.z_dim))
+        
+        # Transform
+        sigma = torch.exp(logsigma)
+        pi = F.softmax(pi, 2)  # Weights over n_mixtures should sum to one
+
+        # add temperature
+        if self.tau > 0:
+            pi /= self.tau
+            sigma *= self.tau ** 0.5
+
+        return pi, mu, sigma
+    
+    def normal_prob(self, y_true, mu, sigma, pi):
+        """Probability of a value given the distribution."""
+        rollout_length = y_true.size(1)
+        
+        # Repeat, for number of repeated mixtures
+        y_true = y_true.unsqueeze(2).repeat((1, 1, self.n_mixture, 1))
+        
+        # Use pytorches normal dist class to calc the probs
+        z_normals = torch.distributions.Normal(mu, sigma)
+        z_prob = z_normals.log_prob(y_true).exp() #.clamp(logeps, -logeps).exp()
+        return (z_prob * pi).sum(2) # weight, then sum over the mixtures
+    
+    def sample(self, pi, mu, sigma):
+        """Sample z from Z."""
+        z_normals = torch.distributions.Normal(mu, sigma)
+        if self.training:
+            z_sample = z_normals.rsample()
+        else:
+            z_sample = z_normals.sample()
+        return (z_sample * pi).sum(2) # sum over mixtures
+
+    def rnn_r_loss(self, y_true, pi, mu, sigma):
+        # See https://github.com/hardmaru/pytorch_notebooks/blob/master/mixture_density_networks.ipynb
+        # and https://github.com/AppliedDataSciencePartners/WorldModels/blob/master/rnn/arch.py#L39
+        # and https://github.com/JunhongXu/world-models-pytorch
+
+        # probability shape [batch, seq, num_mixtures, z_dim]  
+        prob = self.normal_prob(y_true, mu, sigma, pi)
+        loss = -torch.log(prob + eps)
+
+        # mean over seq and z dim and num_mixtures
+        batch_size = y_true.size(0)
+        loss = loss.view((batch_size, -1)).mean(1)
+        return loss
+
+    def rnn_loss(self, y_true, pi, mu, sigma):
+        r_loss = self.rnn_r_loss(y_true, pi, mu, sigma)
+        return r_loss
