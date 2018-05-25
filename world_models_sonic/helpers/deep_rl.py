4062116a235d50e14ac05ec44bc9429ebd72a7a2
wassname <wassname@users.noreply.github.com>
2018-05-25 18:54:13 +0800

    tidy up

--- world_models_sonic/helpers/deep_rl.py
+++ world_models_sonic/helpers/deep_rl.py
@@ -3,16 +3,21 @@ Helpers for https://github.com/ShangtongZhang/DeepRL
 """
 import torch
 import time
-from torch import nn, optim
+from torch import nn
+from torch.nn import functional as F
 import numpy as np
-from tqdm import tqdm
 import pickle
 
+
+import skimage.color
+import cv2
+
+from deep_rl.network.network_utils import BaseNet
 from deep_rl.agent import BaseAgent, Batcher
+from deep_rl.network.network_heads import ActorCriticNet
 from deep_rl.component.task import BaseTask
 
-from ..custom_envs.wrappers import RenderWrapper, WorldModelWrapper
-from ..custom_envs.env import make_env
+from ..custom_envs.wrappers import RenderWrapper
 
 
 class SonicWorldModelDeepRL(BaseTask):
@@ -181,3 +186,143 @@ def run_iterations(agent, log_dir):
             break
 
     return steps, rewards
+
+
+class CategoricalWorldActorCriticNet(nn.Module, BaseNet):
+    """
+    A custom network head with world models and rendering.
+
+    Putting it here instead of as an environement wrapper allows multithreading
+    because it removes all pytorch operations from the rollout phase.
+    """
+
+    def __init__(self,
+                 state_dim,
+                 action_dim,
+                 phi_body=None,
+                 actor_body=None,
+                 critic_body=None,
+                 world_model_fn=None,
+                 gpu=-1,
+                 render=False
+                 ):
+        super().__init__()
+        self.world_model = world_model_fn()
+        self.z_state_dim = self.world_model.mdnrnn.z_dim + self.world_model.mdnrnn.hidden_size
+        self.network = ActorCriticNet(self.z_state_dim, action_dim, phi_body, actor_body, critic_body)
+
+        self.viewer = None
+        self.max_hidden_states = 6
+        self.hidden_state = None
+        self.set_gpu(gpu)
+
+    def predict(self, obs, action=None):
+        self.img = obs
+        with torch.no_grad():
+            # Input is (batch, samples...)
+            obs = self.tensor(obs).transpose(1, 3)
+
+            # Use world model to transform obs
+            if self.hidden_state and (obs.size(0) != self.hidden_state[0].size(0) or obs.size(1) != self.hidden_state[0].size(1)):
+                self.hidden_state = None
+            z_next, z, hidden_state = self.world_model.forward(obs, hidden_state=self.hidden_state)
+
+            hidden_state = [h.data for h in hidden_state]  # Otherwise it doesn't garbge collect
+            self.hidden_state = hidden_state[-self.max_hidden_states:]
+            self.z = z.data
+            self.z_next = z_next.data
+
+            latest_hidden = hidden_state[-1].squeeze(0)  # squeeze so we can concat
+        obs = torch.cat([z, latest_hidden], -1)
+        obs.requires_grad = True
+
+        # Predict next action and value
+        phi = self.network.phi_body(obs)
+        phi_a = self.network.actor_body(phi)
+        phi_v = self.network.critic_body(phi)
+        prob = F.softmax(self.network.fc_action(phi_a), dim=-1)
+        v = self.network.fc_critic(phi_v)
+        dist = torch.distributions.Categorical(probs=prob)
+        if action is None:
+            action = dist.sample()
+            self.render()
+        log_prob = dist.log_prob(action).unsqueeze(-1)
+
+        return action, log_prob, dist.entropy().unsqueeze(-1), v
+
+    def render(self, mode='world_model', close=False):
+        if close:
+            for viewer in [self.viewer_z, self.viewer_z_next, self.viewer_img_z, self.viewer_img_z_next]:
+                viewer.close()
+            if self.viewer:
+                self.viewer.close()
+            return
+        if mode == 'world_model':
+            # Render while showing latent vectors and decoded latent vectors
+            if self.viewer is None:
+                from gym.envs.classic_control.rendering import SimpleImageViewer
+                import pyglet
+                margin_vert = 60  # for window border
+                # TODO add margin_horiz
+                mult = 3
+
+                self.viewer = SimpleImageViewer()
+                self.viewer.window = pyglet.window.Window(width=160 * mult, height=128 * mult, vsync=False, resizable=True, caption='Game output')
+                self.viewer.window.set_location(0 * mult, 0 * mult)
+
+                self.viewer_img_z = SimpleImageViewer()
+                self.viewer_img_z.window = pyglet.window.Window(width=128 * mult, height=128 * mult, vsync=False, resizable=True, caption='Decoded image')
+                self.viewer_img_z.window.set_location(160 * mult, 0 * mult)
+
+                self.viewer_img_z_next = SimpleImageViewer()
+                self.viewer_img_z_next.window = pyglet.window.Window(width=128 * mult, height=128 * mult,
+                                                                     vsync=False, resizable=True, caption='Decoded predicted image')
+                self.viewer_img_z_next.window.set_location((160 + 128) * mult, 0 * mult)
+
+                self.viewer_z = SimpleImageViewer()
+                self.viewer_z.window = pyglet.window.Window(width=128 * mult, height=128 * mult, vsync=False, resizable=True, caption='latent vector')
+                self.viewer_z.window.set_location(160 * mult, 128 * mult + margin_vert)
+
+                self.viewer_z_next = SimpleImageViewer()
+                self.viewer_z_next.window = pyglet.window.Window(width=128 * mult, height=128 * mult, vsync=False,
+                                                                 resizable=True, caption='latent predicted vector')
+                self.viewer_z_next.window.set_location((160 + 128) * mult, 128 * mult + margin_vert)
+
+            # Decode latent vector for display
+            with torch.no_grad():
+
+                # to pytorch
+                zv = self.z
+                zv_next = self.z_next
+                if self.cuda:
+                    zv = zv.cuda()
+                    zv_next = zv_next.cuda()
+
+                # Decode
+                img_z = self.world_model.vae.decode(zv)
+                img_z_next = self.world_model.vae.decode(zv_next)
+
+                # to numpy images
+                img_z = img_z.squeeze(0).transpose(0, 2)
+                img_z = img_z.data.cpu().numpy()
+                img_z = (img_z * 255.0).astype(np.uint8)
+                img_z_next = img_z_next.squeeze(0).transpose(0, 2).clamp(0, 1)
+                img_z_next = img_z_next.data.cpu().numpy()
+                img_z_next = (img_z_next * 255.0).astype(np.uint8)
+
+                z_uint8 = ((self.z[0].data.cpu().numpy() + 0.5) * 255).astype(np.uint8).reshape((16, 16))
+                z_uint8 = skimage.color.gray2rgb(z_uint8)  # Make it rgb to avoid problems with pyglet
+                z_uint8 = cv2.resize(z_uint8, dsize=(256, 256), interpolation=cv2.INTER_NEAREST)  # Resize manually to avoid interp of pixels
+
+                z_next_uint8 = ((self.z_next[0].data.cpu().numpy() + 0.5) * 255).astype(np.uint8).reshape((16, 16))
+                z_next_uint8 = skimage.color.gray2rgb(z_next_uint8)
+                z_next_uint8 = cv2.resize(z_next_uint8, dsize=(256, 256), interpolation=cv2.INTER_NEAREST)
+
+                # Display
+                img = self.img[0] * 255
+                self.viewer.imshow(img.astype(np.uint8))
+                self.viewer_img_z.imshow(img_z)
+                self.viewer_img_z_next.imshow(img_z_next)
+                self.viewer_z.imshow(z_uint8)
+                self.viewer_z_next.imshow(z_next_uint8)
+                return self.viewer.isopen
