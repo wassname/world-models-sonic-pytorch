521b7a04dc3eff96ef6883f7016a28f03feee45a
wassname <wassname@users.noreply.github.com>
2018-06-23 13:26:34 +0800

    smaller inverse model

--- world_models_sonic/models/inverse_model.py
+++ world_models_sonic/models/inverse_model.py
@@ -21,3 +21,21 @@ class InverseModel(torch.nn.modules.Module):
         x = F.relu(self.ln2(x))
         x = self.ln3(x)
         return x
+
+
+class InverseModel2(torch.nn.modules.Module):
+    def __init__(self, z_dim, action_dim, hidden_size):
+        """
+        Inverse model from https://arxiv.org/abs/1804.10689.
+
+        Takes in z and z' and outputs predicted action
+        """
+        super().__init__()
+        self.ln1 = nn.Linear(z_dim * 2, hidden_size)
+        self.ln3 = nn.Linear(hidden_size, action_dim)
+
+    def forward(self, z_now, z_next):
+        x = torch.cat((z_now, z_next), dim=-1)
+        x = F.relu(self.ln1(x))
+        x = self.ln3(x)
+        return x
